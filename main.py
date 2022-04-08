2a99f52afaac312d455a53c9dcb4c65e17409b27
joaor1os <101368524+joaor1os@users.noreply.github.com>
2022-04-08 00:36:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect
+app = Flask('app')
+
+contacts = []
+
+@app.route('/')
+def index():
+  return render_template(
+    'index.html',
+    contacts=contacts
+  )
+
+@app.route('/create', methods=['POST'])
+def create():
+  name = request.form.get('name')
+  email = request.form.get('email')
+  phone = request.form.get('phone')
+  contacts.append({
+    'name': name,
+    'email': email,
+    'phone': phone,
+  })
+  return redirect('/')
+
+@app.route('/delete/<int:index>')
+def delete(index):
+  contacts.pop(index)
+  return redirect('/')
+
+@app.route('/update/<int:index>', methods=['POST'])
+def update(index):
+  name = request.form.get('name')
+  contacts[index]['name'] = name
+  email = request.form.get('email')
+  contacts[index]['email'] = email
+  phone = request.form.get('phone')
+  contacts[index]['phone'] = phone
+  return redirect('/')
+
+# IMPORTANTE V
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
